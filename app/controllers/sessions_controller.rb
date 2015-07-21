65d99cfcd95083557302cba09dcfaae7aedef771
Jan <pribosek.jan@gmail.com>
2015-07-21 02:00:14 +0200

    Finish log in /log out + remember me checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,8 @@ class SessionsController < ApplicationController
   	user = User.find_by(nickname: params[:session][:nickname])
   	if user && user.authenticate(params[:session][:password])
   		log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   		redirect_to user
-
   	else
   		flash.now[:danger] = "Invalid nickname/password combination"
   		render 'new'
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-
+attr_accessor :remember_token
 before_save {self.email = email.downcase}
+
 
 	validates :nickname, presence: true, length: {maximum: 50}, uniqueness: { case_sensitive: false }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -18,5 +19,19 @@ has_secure_password
   def User.new_token
   	SecureRandom.urlsafe_base64
   end
+
+  def remember
+  	self.remember_token = User.new_token
+  	update_attribute(:remember_digest, User.digest(remember_token))
+  end
   
+  def authenticated?(remember_token)
+  	return false if remember_digest.nil?
+  	 BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+  	update_attribute(:remember_digest, nil)
+  end
+
 end
