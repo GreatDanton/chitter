728fbdb11c3883a782cc6b1b0536fe1949c9f618
Jan <pribosek.jan@gmail.com>
2015-07-29 01:15:09 +0200

    fail

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,56 +1,67 @@
 class DiscussionsController < ApplicationController
-	
-	layout "pages", except: [:frontpage]
+  layout 'pages', only: [:frontpage]
+  before_action :set_discussion, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, except: [:index, :frontpage]
 
-	before_action :find_discussion, only: [:show, :edit, :update, :destroy]
+  # GET /discussions
+  # GET /discussions.json
+  def index
+    @discussions = Discussion.all.order(created_at: :desc)
+  end
+
+  # GET /discussions/1
+  # GET /discussions/1.json
+
+  # GET /discussions/new
+  def new
+    @discussion = current_user.discussions.build
+  end
+
+  # GET /discussions/1/edit
+  def edit
+  end
+
+  # POST /discussions
+  # POST /discussions.json
+  def create
+    @discussion = current_user.discussions.build(discussion_params)
 
 
-	def index
-		@discussions = Discussion.all.order("created_at DESC")
-	end
+      if @discussion.save
+        redirect_to discussions_path
+        flash[:success] = "Discussion was successfully created."
+      else
+        render 'new'
+    end
+  end
 
-	def show
-	
-	end
+  # PATCH/PUT /discussions/1
+  # PATCH/PUT /discussions/1.json
+  def update
+    if @discussion.update(discussion_params)
+      redirect_to discussions_path
+      flash[:success] = "Discussion was successfully updated."
+    else
+      render 'edit'
+    end
+  end
 
-	def new
-		@discussion = Discussion.new
-	end
+  # DELETE /discussions/1
+  # DELETE /discussions/1.json
+  def destroy
+    @discussion.destroy
+    flash[:success] = "Discussion was successfully deleted."
+    redirect_to discussions_path
+    end
 
-	def create
-		@discussion = Discussion.new(discussion_params)
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_discussion
+      @discussion = Discussion.find(params[:id])
+    end
 
-		if @discussion.save
-			redirect_to discussions_path
-		else
-			render 'new'
-		end
-	end
-
-	def edit
-
-	end
-
-	def update
-		if @discussion.update(discussion_params)
-			redirect_to @discussion
-		else
-			render 'edit'
-		end
-	end
-
-	def destroy
-		@discussion.destroy
-		redirect_to discussions_path
-	end
-
-	private
-	def find_discussion
-		@discussion = Discussion.find(params[:id])
-	end
-
-	def discussion_params
-		params.require(:discussion).permit(:content)
-	end
-
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def discussion_params
+      params.require(:discussion).permit(:content)
+    end
 end
